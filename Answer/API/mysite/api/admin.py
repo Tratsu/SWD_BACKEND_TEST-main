b7a393a7dd5b09dbfd63317b151c88acc5596e86
Tratsu <130635483+Tratsu@users.noreply.github.com>
2023-04-13 16:06:42 +0700

    Delete Answer directory

--- Answer/API/mysite/api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import TodoList as Task
-
-
-# Register our model
-admin.site.register(Task)
--- Answer/API/mysite/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TaskList, TaskDetail
-
-urlpatterns = [
-    path('', TaskList.as_view(), name='todo_list_create'),
-    path('<int:pk>/', TaskDetail.as_view(), name='todo_retrieve_update_delete'),
-]
